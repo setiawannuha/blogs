452c7d1394f7a2673729b904a75a75411dbb9de4
Setiawan restu aji <setiawannuha123@gmail.com>
2021-04-12 12:30:49 +0700

    responsive

--- resources/views/template/single.blade.php
+++ resources/views/template/single.blade.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <title>Woroworo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'/>
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}"> --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
